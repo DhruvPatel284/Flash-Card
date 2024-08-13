1f4a666ab5f6a756e5447f5cf8ecbdd27919608f
DhruvPatel284 <dhruv156328@gmail.com>
2024-08-13 18:59:01 +0530

    mostly tackled the bug

--- flash-card/src/components/Navbar.tsx
+++ flash-card/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import Link from "next/link";
 import axios from 'axios'; // Make sure axios is installed
+import { cookies } from "next/headers";
 
 interface MyToken {
   name: string;
@@ -60,51 +61,38 @@ export const Navbar = () => {
 
   return (
     <nav className="p-4 w-full shadow-md shadow-purple-800">
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container mx-auto flex items-center justify-between flex-wrap">
         <Link href="/" className="text-black text-xl font-semibold items-center ml-3 flex">
-          <div className='h-[30px] mr-2'>
-            <img className='rounded h-[30px]' src="../images/articlenew.jpg" alt="" />
-          </div>
           <div className="text-2xl text-purple-600">
-              Flash-Card
+            Card-Components
           </div>
         </Link>
-        <div className="flex items-center justify-center space-x-4">
+        <div className="flex items-center justify-center space-x-4 mt-4 md:mt-0">
           {isLoggedIn ? (
             <>
               {isAdmin && (
-                <div className='flex'>
                 <Link
                   href="/publish"
-                  className="mr-4 text-black bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 transition duration-150 ease-in-out"
+                  className="text-black bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out"
                 >
                   New
                 </Link>
-                  <Link
-                   href="/dashboard"
-                   className="mr-4 text-black bg-blue-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 transition duration-150 ease-in-out"
-                   >
-                     Dashboard
-                  </Link>
-                  </div>
               )}
-              <div className="">
-                <button onClick={toggleDropdown} className="focus:outline-none mb-2">
+              {isAdmin && (
+                <Link
+                  href="/dashboard"
+                  className="text-black bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out"
+                >
+                  Dashboard
+                </Link>
+              )}
+              <div className="relative">
+                <button onClick={toggleDropdown} className="focus:outline-none">
                   <Avatar size={"big"} name={username} />
                 </button>
                 {dropdownVisible && (
-                  <div className="absolute right-0 mt-2 w-48 mr-1 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 transition-transform transform duration-200 origin-top-right">
+                  <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 transition-transform transform duration-200 origin-top-right">
                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                      {isAdmin && (
-                        <Link
-                          href='/dashboard'
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition duration-150 ease-in-out"
-                          role="menuitem"
-                        >
-                         Dashboard
-                        </Link>
-                      )}
-                      <div className='h-[0.1px] bg-black'></div>
                       <button
                         onClick={handleLogout}
                         className="block w-full text-left px-4 py-2 text-sm text-red-700 hover:bg-gray-100 hover:text-gray-900 transition duration-150 ease-in-out"
@@ -120,7 +108,7 @@ export const Navbar = () => {
           ) : (
             <Link
               href="/signin"
-              className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 transition duration-150 ease-in-out"
+              className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center transition duration-150 ease-in-out"
             >
               Log in
             </Link>
@@ -133,8 +121,8 @@ export const Navbar = () => {
 
 export function Avatar({ name, size = "small" }: { name: string, size?: "small" | "big" }) {
   return (
-    <div className={`flex justify-center bg-gray-400 rounded-full dark:bg-gray-600 ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
-      <span className={`${size === "small" ? "text-xs" : "text-xl"} m-auto font-extralight text-white dark:text-gray-300`}>
+    <div className={`flex justify-center bg-purple-800 rounded-full dark:bg-gray-600 ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
+      <span className={`${size === "small" ? "text-xs" : "text-xl"} m-auto font-extralight text-black dark:text-gray-300`}>
         {name[0]}
       </span>
     </div>
