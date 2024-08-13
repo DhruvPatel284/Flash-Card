4cadc1e331652546ed66f5907c39b109e0dc94d8
DhruvPatel284 <dhruv156328@gmail.com>
2024-08-13 16:56:57 +0530

    trying to solve problem of caching

--- flash-card/src/app/api/FlashCard/getFlashcard/route.ts
+++ flash-card/src/app/api/FlashCard/getFlashcard/route.ts
@@ -15,17 +15,18 @@
 import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
+// Disable caching for this API route
+export const fetchCache = 'force-no-store';
+
 export async function GET(req: Request) {
   try {
     const flashcards = await prismadb.flashcard.findMany();
 
-    const response = NextResponse.json(flashcards);
-    response.headers.set('Cache-Control', 'no-store');
-
-    return response;
+    return NextResponse.json(flashcards);
   } catch (error) {
     console.log(error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
+
 
